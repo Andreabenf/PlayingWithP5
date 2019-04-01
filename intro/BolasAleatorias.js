a22515e83cfebd3e1e074f2be2ec9b4922a9c27a
andreabenf <andreabenf@gmail.com>
2019-04-01 09:07:25 -0300

    Bolas aleatorias feito

--- intro/BolasAleatorias.js
+++ intro/BolasAleatorias.js
@@ -1,40 +1,39 @@
-let x=0
+let x = 0
 let roger = []
 
 function setup() {
     createCanvas(1800, 900);
-    
-    for(let i=0;i<100;i++){
-        let r = random(1,200)
-        let x = random(0,1800)
-        let y = random(0,900)
-        let top = new Bola(x,y,r)
+
+    for (let i = 0; i < 100; i++) {
+        let r = random(1, 200)
+        let x = random(0, 1800)
+        let y = random(0, 900)
+        let top = new Bola(x, y, r)
         roger.push(top)
     }
-    
+
 }
 
 function draw() {
     background(150, 20, 200)
-    for(let i =0;i<roger.length;i++){
-    roger[i].existe()
+    for (let i = 0; i < roger.length; i++) {
+        roger[i].existe()
     }
 
 }
 
 class Bola {
-    constructor(x,y,r){
+    constructor(x, y, r) {
         this.x = x
         this.y = y
         this.r = r
     }
 
-    existe(){
-        fill(random(0,255),random(0,255),random(0,255))
-
+    existe() {
         ellipse(this.x, this.y, this.r)
-            this.x ++
-            this.y --}
-    
-  
+        let novx = random(0, 1800)
+        let novy = random(0, 900)
+    }
+
+
 }
