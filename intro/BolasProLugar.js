7785e6f7f8e2248ee358f67932013c3867403179
andreabenf <andreabenf@gmail.com>
2019-04-01 21:09:54 -0300

    lhul

--- intro/BolasProLugar.js
+++ intro/BolasProLugar.js
@@ -5,9 +5,9 @@ function setup() {
     createCanvas(1800, 900);
 
 
-    let top = new Bola(100, height / 2, 50)
+    let top = new Bola(random(10, 500), random(10, 500), 50)
     roger.push(top)
-    let tope = new Bola(500, height / 2, 50)
+    let tope = new Bola(random(10, 500), random(10, 500), 50)
     roger.push(tope)
 
 }
@@ -18,32 +18,44 @@ function draw() {
     for (let i = 0; i < roger.length; i++) {
         roger[i].existe()
     }
-    swape(roger[0],roger[1])
-}
+   
 
-class Bola {
-    constructor(x, y, r) {
-        this.x = x
-        this.y = y
-        this.r = r
-    }
-
-    existe() {
-        fill(223, 90)
-        ellipse(this.x, this.y, this.r)
-        let novx = random(0, 1800)
-        let novy = random(0, 900)
-    }
-
-
-}
-
-function swape(a, b) {
-    if (a.x != b.x) {
-        if (a.x > b.x) {
-            a.x--
-        } else {
-            a.x++
+    class Bola {
+        constructor(x, y, r) {
+            this.x = x
+            this.y = y
+            this.r = r
         }
+
+        existe() {
+            fill(223, 90)
+            ellipse(this.x, this.y, this.r)
+            let novx = random(0, 1800)
+            let novy = random(0, 900)
+        }
+
+        swape(b) {
+            if (this.x != b.x) {
+                if (this.x > b.x) {
+                    this.x--
+                } else {
+                    this.x++
+                }
+            }
+            if (this.y != b.y) {
+                if (this.y > b.y) {
+                    this.y--
+                } else {
+                    this.y++
+                }
+            }
+        }
+
+
     }
-}
+
+   
+
+    function mouseClicked() {
+        return true;
+    }
